b1527d6b08ced665ecfefef918825e788ec30d44
AMarchmarti <marchmarti.antoni@gmail.com>
2019-03-27 20:31:01 +0100

    Empezar el calculo de valores

--- src/main/java/org/lasencinas/RomanosMain.java
+++ src/main/java/org/lasencinas/RomanosMain.java
@@ -1,12 +1,15 @@
 package org.lasencinas;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class RomanosMain{
 
     public static Boolean verificarString(String numero){
         String regex = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})";
-        return numero.matches(regex);    }
+        return numero.matches(regex);
+    }
+
+    public static Integer calculoNumerosRomanos(String numero){
+        String regexSumatorio = "^M{0,3}(D?C{0,3})(L?X{0,3})(V?I{0,3})";
+    }
 }
