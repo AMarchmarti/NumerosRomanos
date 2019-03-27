26c998b3f5454a129519098e457895340c711440
AMarchmarti <marchmarti.antoni@gmail.com>
2019-03-27 17:57:22 +0100

    Test constructor aplicado

--- src/test/java/org/lasencinas/RomanosTest.java
+++ src/test/java/org/lasencinas/RomanosTest.java
@@ -20,7 +20,20 @@ public class RomanosTest{
         assertThat(numerosRomanos).isEqualTo(Romanos.values().length);
     }
 
-
+    @Test
+    public void RomanosConstructorTest() {
+        // voy a asegurarme de que los metodos de los Enum Types
+        // se comportan como yo espero
+        Romanos numeroRomano = Romanos.M;
+        assertThat(numeroRomano).isInstanceOf(Romanos.class);
+        assertThat(numeroRomano.ordinal()).isEqualTo(6);
+        assertThat(numeroRomano.name()).isEqualToIgnoringWhitespace("M");
+        assertThat(Romanos.valueOf(numeroRomano.name())).isEqualTo(Romanos.M);
+        assertThat(numeroRomano.compareTo(numeroRomano.M)).isEqualTo(0);
+        assertThat(numeroRomano.toString()).isEqualToIgnoringWhitespace("M");
+        assertThat(numeroRomano.equals(numeroRomano.M)).isEqualTo(true);
+        assertThat(Romanos.values()[6]).isEqualTo(numeroRomano);
+    }
     @Test
     public void getNumeroDecimalTest(){
         Romanos numeroRomano = Romanos.M;
